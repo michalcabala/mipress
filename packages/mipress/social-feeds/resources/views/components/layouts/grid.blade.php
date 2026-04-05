4705012a3e885c57c02f509d7cf6d61d68b53f5b
michalcabala <michal@cabala.cz>
2026-04-05 15:21:36 +0200

    chore: remove social-feeds from main repo tracking (now separate repo)

--- packages/mipress/social-feeds/resources/views/components/layouts/grid.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-    $columns = $feed->settings['columns'] ?? 3;
-@endphp
-<div class="sf-grid grid gap-4" style="grid-template-columns: repeat({{ $columns }}, 1fr);">
-    @foreach($posts as $post)
-        @include('social-feeds::components.post-card', ['post' => $post, 'feed' => $feed])
-    @endforeach
-</div>

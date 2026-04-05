dbe4702cb5ad78aae809322812c53584b4d06d1c
michalcabala <michal@cabala.cz>
2026-04-05 14:39:02 +0200

    fix: use Facebook Login for Business scopes as defaults

--- packages/mipress/social-feeds/config/social-feeds.php
+++ packages/mipress/social-feeds/config/social-feeds.php
@@ -13,7 +13,7 @@ return [
             'client_id' => env('FACEBOOK_CLIENT_ID'),
             'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
             'graph_version' => env('FACEBOOK_GRAPH_VERSION', 'v23.0'),
-            'scopes' => array_filter(explode(',', env('FACEBOOK_SCOPES', 'public_profile,email'))),
+            'scopes' => array_filter(explode(',', env('FACEBOOK_SCOPES', 'pages_show_list,pages_read_engagement,pages_read_user_content'))),
         ],
         'instagram' => [
             'enabled' => env('SOCIAL_INSTAGRAM_ENABLED', false),

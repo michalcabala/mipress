a63630b1fc71e6a5ea961da5ba304599eb65e3eb
michalcabala <michal@cabala.cz>
2026-04-12 18:46:04 +0200

    fix: override form() in CuratorMediaResource to use CuratorMediaForm

--- app/Filament/Resources/CuratorMediaResource.php
+++ app/Filament/Resources/CuratorMediaResource.php
@@ -7,11 +7,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CuratorMediaResource\Pages\CreateCuratorMedia;
 use App\Filament\Resources\CuratorMediaResource\Pages\EditCuratorMedia;
 use App\Filament\Resources\CuratorMediaResource\Pages\ListCuratorMedia;
+use App\Filament\Resources\CuratorMediaResource\Schemas\CuratorMediaForm;
 use Awcodes\Curator\Resources\Media\MediaResource;
+use Filament\Schemas\Schema;
 
 class CuratorMediaResource extends MediaResource
 {
     protected static ?string $slug = 'curator-media';
+
+    public static function form(Schema $schema): Schema
+    {
+        return CuratorMediaForm::configure($schema);
+    }
 
     public static function getPages(): array
     {
